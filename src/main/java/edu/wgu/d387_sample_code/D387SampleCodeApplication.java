8b9a8de19105ca31b97249ede23400768a1bdb14
jluo <jluo8@wgu.edu>
2023-09-15 22:20:50 -0700

    update

--- src/main/java/edu/wgu/d387_sample_code/D387SampleCodeApplication.java
+++ src/main/java/edu/wgu/d387_sample_code/D387SampleCodeApplication.java
@@ -3,14 +3,9 @@ package edu.wgu.d387_sample_code;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.concurrent.ExecutorService;
-
-import static java.util.concurrent.Executors.newFixedThreadPool;
-
 @SpringBootApplication
 public class D387SampleCodeApplication {
 
-  static ExecutorService messageExecutor = newFixedThreadPool(5);
 
   public static void main(String[] args) {
 
@@ -39,24 +34,6 @@ public class D387SampleCodeApplication {
 //      }
 //    });
 
-
-//    ZoneId zEastern = ZoneId.of("America/New_York");
-//
-//    ZoneId zPacific = ZoneId.of("America/Los_Angeles");
-//    ZoneId zoneId = ZoneId.systemDefault();
-//
-//    LocalDateTime localDateTime = LocalDateTime.now();
-//    System.out.println("local time now: " + localDateTime.toString());
-//
-//    ZonedDateTime zonedDateTime = localDateTime.atZone(zoneId);
-//
-//    ZonedDateTime zonedDateTimeEastern = zonedDateTime.withZoneSameInstant(zEastern);
-//    LocalDateTime localDateTimeEastern = zonedDateTimeEastern.toLocalDateTime();
-//    System.out.println("local time now in Eastern: " + localDateTimeEastern.toString());
-//
-//    ZonedDateTime zonedDateTimePacific = zonedDateTime.withZoneSameInstant(zPacific);
-//    LocalDateTime localDateTimePacific = zonedDateTimePacific.toLocalDateTime();
-//    System.out.println("local time now in Pacific time " + localDateTimePacific.toString());
 
   }
 }

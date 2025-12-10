e2fbdbea5f4cb5389101c02d7a2b66b300b7d858
jluo <jluo8@wgu.edu>
2025-12-10 04:40:50 -0800

    Refactor code structure for improved readability and maintainability

--- src/main/java/edu/wgu/d387_sample_code/D387SampleCodeApplication.java
+++ src/main/java/edu/wgu/d387_sample_code/D387SampleCodeApplication.java
@@ -1,23 +1,36 @@
 package edu.wgu.d387_sample_code;
 
 
+import edu.wgu.d387_sample_code.model.MyWelcomeMessage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @SpringBootApplication
 public class D387SampleCodeApplication {
 
-
   public static void main(String[] args) {
 
     SpringApplication.run(D387SampleCodeApplication.class, args);
+    ExecutorService executor = Executors.newFixedThreadPool(4);
+
+
+    MyWelcomeMessage messages = new MyWelcomeMessage();
+
+    executor.execute(() -> {
+      messages.welcome().getEnglishMessage();
+      System.out.println(messages.welcome().getEnglishMessage() + " " + Thread.currentThread().getName());
+    });
+
+    executor.execute(() -> {
+      messages.welcome().getFrenchMessage();
+      System.out.println(messages.welcome().getFrenchMessage() + " " + Thread.currentThread().getName());
+    });
 
 
   }
-
-
-
-
 }
 
 
